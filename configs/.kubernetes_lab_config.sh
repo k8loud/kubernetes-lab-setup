60c6d8e220ad1cd0c70100fd478c786e37e87f79
proman3419 <proman7268@gmail.com>
2023-03-22 17:55:36 +0100

    Restored functions connecting to specific vm

--- configs/.kubernetes_lab_config.sh
+++ configs/.kubernetes_lab_config.sh
@@ -5,7 +5,6 @@ KUBE3_IP="192.168.1.30"
 
 alias VBoxManage="$VBOX_MANAGE_PATH"
 
-
 function vm-on { 
   VBoxManage startvm "$1" --type headless 
 }
@@ -19,12 +18,23 @@ function vm-ssh {
 }
 export -f vm-ssh
 
-
+function vm-kube1-ssh {
+  vm-ssh "$KUBE1_IP"
+}
+export -f vm-kube1-ssh
 alias vm-kube1-on="vm-on kube1"
 alias vm-kube1-off="vm-off kube1"
 
+function vm-kube2-ssh {
+  vm-ssh "$KUBE2_IP"
+}
+export -f vm-kube2-ssh
 alias vm-kube2-on="vm-on kube2"
 alias vm-kube2-off="vm-off kube2"
 
+function vm-kube3-ssh {
+  vm-ssh "$KUBE3_IP"
+}
+export -f vm-kube3-ssh
 alias vm-kube3-on="vm-on kube3"
 alias vm-kube3-off="vm-off kube3"
