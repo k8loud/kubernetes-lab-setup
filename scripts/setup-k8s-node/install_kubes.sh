e4ac2e12a2e3cdf246a0300697d1cfbcc02fe7df
Paweł Pycia <45673406+Agwen12@users.noreply.github.com>
2023-04-15 23:55:13 +0200

    freez kubelet and kubectl version

--- scripts/setup-k8s-node/install_kubes.sh
+++ scripts/setup-k8s-node/install_kubes.sh
@@ -8,7 +8,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update -y
-sudo apt install wget curl vim git kubelet kubeadm kubectl -y
+sudo apt install wget curl vim git kubelet=1.26.3-00 kubeadm kubectl=1.26.3-00 -y
 sudo apt-mark hold kubelet kubeadm kubectl -y
 
 kubectl version --client && kubeadm version
