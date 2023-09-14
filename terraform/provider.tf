7cbcaf22c357e09010db40811aa247d1149c73c9
Agwen12 <pawel.pycia29@gmail.com>
2023-09-14 19:11:01 +0200

    add basic terraform scripts

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "~> 1.52.1"
+    }
+  }
+}
+
+provider "openstack" {
+  user_name = "plgppycia"
+  auth_url = "https://api.cloud.cyfronet.pl:5000/v3"
+  region = "RegionOne"
+  user_domain_name = "Default"
+  enable_logging = true
+}
