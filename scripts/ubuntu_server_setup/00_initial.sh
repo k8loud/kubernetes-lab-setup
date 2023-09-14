5b132328cd4b4876e16af34e8eb4cd2bb65e6732
proman3419 <proman7268@gmail.com>
2023-09-15 00:04:13 +0200

    Fixed repo url

--- scripts/ubuntu_server_setup/00_initial.sh
+++ scripts/ubuntu_server_setup/00_initial.sh
@@ -6,6 +6,6 @@ sudo apt-get install openssh-server
 
 ssh-keygen -o -b 4096 -t rsa
 
-git clone https://raw.githubusercontent.com/k8loud/kubernetes-lab-setup.git ~/kubernetes-lab-setup
+git clone https://github.com/k8loud/kubernetes-lab-setup.git ~/kubernetes-lab-setup
 
 # Shutdown and change the adapter's settings
