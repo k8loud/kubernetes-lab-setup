08d1af86a32cfac8ce58bcf1c9c06dee9b6a25dd
proman3419 <proman7268@gmail.com>
2023-09-17 13:35:52 +0200

    Fixed missing DNS

--- scripts/ubuntu_server_setup/00_initial.sh
+++ scripts/ubuntu_server_setup/00_initial.sh
@@ -1,8 +1,17 @@
 #!/bin/bash 
 
+# This won't work long time, config in this path will be overriden
+# Temporary fix so resolvconf can be downloaded
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+sudo apt -y install resolvconf
+
+sudo mkdir -p /etc/resolvconf/resolv.conf.d
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+sudo resolvconf -u
+
 sudo apt update
 sudo apt -y upgrade
-sudo apt-get install openssh-server
+sudo apt-get -y install openssh-server
 
 ssh-keygen -o -b 4096 -t rsa
 
