99e5bbc1f8a2644b8bc6ee5f17603aaaf609af36
proman3419 <proman7268@gmail.com>
2023-10-09 00:10:42 +0200

    Services fixes 2

--- scripts/kube_setup/04m_setup_push_token_args_service.sh
+++ scripts/kube_setup/04m_setup_push_token_args_service.sh
@@ -4,6 +4,7 @@ sudo apt -y install nmap
 sudo apt -y install ucspi-tcp
 
 sudo cp /home/ubuntu/kubernetes-lab-setup/configs/push_token_args_listener.service /etc/systemd/system/push_token_args_listener.service
-sudo chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/push_token_args_listener.sh
-sudo chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/push_token_args.sh
-systemctl enable push_token_args_listener.service
+chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/push_token_args_listener.sh
+chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/push_token_args.sh
+sudo systemctl enable push_token_args_listener.service
+sudo systemctl start push_token_args_listener.service
--- scripts/kube_setup/05w_setup_join_cluster_service.sh
+++ scripts/kube_setup/05w_setup_join_cluster_service.sh
@@ -3,5 +3,6 @@
 sudo apt -y install nmap
 
 sudo cp /home/ubuntu/kubernetes-lab-setup/configs/join_cluster.service /etc/systemd/system/join_cluster.service
-sudo chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/join_cluster.sh
-systemctl enable join_cluster.service
+chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/join_cluster.sh
+sudo systemctl enable join_cluster.service
+sudo systemctl start join_cluster.service
