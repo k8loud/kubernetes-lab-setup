b6b1dae7565e4ada4adc082d899ca7ed8d3bfc65
proman3419 <proman7268@gmail.com>
2023-10-09 00:57:55 +0200

    Services fixes 3

--- scripts/kube_setup/join_cluster.sh
+++ scripts/kube_setup/join_cluster.sh
@@ -62,6 +62,9 @@ function join_cluster() {
   latest_token="${parts[1]}"
   cert_hash="${parts[2]}"
 
+  echo "Running 'kubeadm reset' (reset the worker's kube config)"
+  echo "y" | kubeadm reset
+
   cmd=$(echo "kubeadm join $api_url --token $latest_token --discovery-token-ca-cert-hash sha256:$cert_hash")
   eval $cmd
 }
