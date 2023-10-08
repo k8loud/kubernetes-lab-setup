563df6dd91e567854aea4a6102abfdf3cd414789
proman3419 <proman7268@gmail.com>
2023-10-08 23:17:04 +0200

    Added missing sudos

--- scripts/kube_setup/05w_setup_join_cluster_service.sh
+++ scripts/kube_setup/05w_setup_join_cluster_service.sh
@@ -3,5 +3,5 @@
 sudo apt -y install nmap
 
 sudo cp /home/ubuntu/kubernetes-lab-setup/configs/join_cluster.service /etc/systemd/system/join_cluster.service
-chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/join_cluster.sh
+sudo chmod +x /home/ubuntu/kubernetes-lab-setup/scripts/kube_setup/join_cluster.sh
 systemctl enable join_cluster.service
