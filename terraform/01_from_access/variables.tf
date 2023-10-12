9f10808dbf0b1c277128465e8a196f3debc2a511
Agwen12 <pawel.pycia29@gmail.com>
2023-10-12 22:13:53 +0200

    add floating IP and extra sans

--- terraform/01_from_access/variables.tf
+++ terraform/01_from_access/variables.tf
@@ -1,11 +1,16 @@
 variable "openstack_username" {
   description = "OpenStack username"
   type        = string
-  sensitive   = true
 }
 
 variable "openstack_password" {
   description = "OpenStack password"
   type        = string
   sensitive   = true
+}
+
+variable "master_ip" {
+  description = "IP address for master node"
+  type = string
+  default = "149.156.10.131"
 }
