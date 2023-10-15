3409b54d574180293c603c607091dd479802677a
proman3419 <proman7268@gmail.com>
2023-10-15 16:08:44 +0200

    Added random suffix to worker node's hostname

--- terraform/01_from_access/variables.tf
+++ terraform/01_from_access/variables.tf
@@ -14,3 +14,9 @@ variable "master_ip" {
   type = string
   default = "149.156.10.131"
 }
+
+variable "worker_hostname_suffix_length" {
+  description = "Length of random suffix appended to Kube worker node's hostname"
+  type = number
+  default = 16
+}
