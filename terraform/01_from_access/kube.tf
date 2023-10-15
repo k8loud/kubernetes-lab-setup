0583c92f090964ce46fa964ec5a974f7a5261ea1
proman3419 <proman7268@gmail.com>
2023-10-15 16:22:44 +0200

    Changed allowed suffix chars to lowercase chars and numbers only

--- terraform/01_from_access/kube.tf
+++ terraform/01_from_access/kube.tf
@@ -1,7 +1,3 @@
-resource "random_string" "worker-suffix" {
-  length = var.worker_hostname_suffix_length
-}
-
 resource "openstack_compute_instance_v2" "kube_master" {
   name = "kube_master"
   image_name = "Ubuntu-Server-22.04-20230914"
@@ -22,15 +18,19 @@ resource "openstack_compute_instance_v2" "kube_master" {
   user_data = file("../../scripts/gen/target/super_script_master.sh")
 }
 
+resource "random_string" "worker-suffix" {
+  length = var.worker_hostname_suffix_length
+  special = false
+  upper = false
+}
 
 resource "openstack_compute_instance_v2" "kube_worker" {
+  depends_on = [openstack_compute_instance_v2.kube_master]
+
   name = "kube_worker-${random_string.worker-suffix.result}"
   image_name = "Ubuntu-Server-22.04-20230914"
   flavor_name = "h2.medium"
   key_pair = "default"
-
-  # to make sure that master is created first?
-#  depends_on = [openstack_compute_instance_v2.kube_master]
 
   security_groups = [
     "default"
